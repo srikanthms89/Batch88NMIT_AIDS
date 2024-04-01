ec2c8d5c823a0727c40213c460514b32273de94e
srikanthms89 <srikanthms@tnsif.org>
2024-04-01 14:42:53 +0530

    First commit

---
+++ NmitBatch88/src/com/tnsif/day2/ForLoopExample.java
@@ -0,0 +1,12 @@
+package com.tnsif.day2;
+
+public class ForLoopExample {
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=1 ; i--)  
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+	}
+}
